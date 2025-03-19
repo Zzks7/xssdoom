9ae7d7d3b3784c879d6c62d9f51660ddf7e44f9c
Zzks7 <161779467+Zzks7@users.noreply.github.com>
2025-03-19 20:52:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function ShowTextOnDOM(){
 
 function CreateGameIframe() {
     var iframe = document.createElement('iframe');
-    iframe.src = 'https://dos.zone/player/?bundleUrl=https%3A%2F%2Fcdn.dos.zone%2Fcustom%2Fdos%2Fdoom.jsdos?anonymous=1'
+    iframe.src = 'https://dos.zone/multiplayer/doom-ii/'
     iframe.id = "Doom"
 
     iframe.height = GameResolutionHeight;
